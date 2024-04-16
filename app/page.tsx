cd89d3c334ac369aacafac7fb9275a986f76abae
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2024-04-16 16:30:11 +0700

    Fix responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,14 @@ import readingList from "./content/reading-list.json";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24 font-mono text-sm">
-			<div className="z-10 w-full max-w-5xl items-center justify-between lg:flex">
+		<main className="flex min-h-screen flex-col items-center justify-between  font-mono text-sm">
+			<div className="z-10 w-full max-w-5xl items-center justify-between lg:flex py-24 px-8 md:px-24">
 				<div className="flex flex-col space-y-8">
 					<AboutSection />
 					<ReadingListSection list={readingList} />
 				</div>
 			</div>
-			<div className="fixed bottom-0">
-				<Foother />
-			</div>
+			<Foother />
 		</main>
 	);
 }
