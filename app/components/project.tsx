88bb83727a3412942d6570d81da0364f9abb3ffe
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2024-04-18 23:04:01 +0800

    Add project section

---
+++ app/components/project.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { FC } from "react";
+import { LinkIcon } from "./icons";
+
+interface ProjectProps {
+	name: string;
+	description: string;
+	link?: string;
+	image: string;
+}
+
+export const Project: FC<ProjectProps> = ({ name, description, link, image }) => {
+	return (
+		<div className="max-w-sm rounded overflow-hidden bg-zinc-900 shadow-zinc-700 shadow-md">
+			<Image className="w-full object-cover" src={image} width={2048} height={1152} alt={name} />
+			<div className="px-6 py-4">
+				<div className="flex flex-row">
+					<div className="font-bold text-xl mb-2">{name}</div>
+					{link && (
+						<a href={link} target="_blank" className="pl-2">
+							<LinkIcon size={20} />
+						</a>
+					)}
+				</div>
+				<div className="text-sm text-gray-400 ">{description}</div>
+			</div>
+		</div>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
 import { AboutSection } from "./components/abount";
+import { Project } from "./components/project";
 import { ReadingListSection } from "./components/reading-list";
 import { Tweet } from "./components/tweet";
+import projects from "./content/projects.json";
 import readingList from "./content/reading-list.json";
 import tweets from "./content/tweets.json";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col space-y-8">
+		<div className="flex flex-col space-y-16">
 			<AboutSection />
 			<ReadingListSection list={readingList} />
 			<div>
-				<h2 className="text-lg head font-bold pb-1">Some random thoughts:</h2>
+				<h2 className="text-lg head font-bold pb-2">Some random thoughts:</h2>
 				<div className="flex flex-col space-y-4">
 					{tweets.map(({ id, text, date }) => (
 						<Tweet key={id} id={id} text={text} date={date} />
+					))}
+				</div>
+			</div>
+			<div>
+				<h2 className="text-lg head font-bold pb-2">Cool stuff I've built:</h2>
+				<div className="grid grid-cols-2 auto-rows-auto gap-8">
+					{projects.map((project) => (
+						<Project key={project.name} {...project} />
 					))}
 				</div>
 			</div>
