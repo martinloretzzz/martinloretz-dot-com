cb2289648f509781b11bef0590a99a5ead32907d
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2024-05-05 21:51:10 +0800

    Fix build action

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: "export",
 	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+	images: {
+		unoptimized: true,
+	},
 };
 
 const withMDX = createMDX({});
