2911eafa77210bac37a18d2f754b0dd7d639f013
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2024-12-27 16:37:19 +0100

    Add tweets

--- app/components/tweet.tsx
+++ app/components/tweet.tsx
@@ -12,19 +12,29 @@ export const Tweet: FC<TweetProps> = ({ id, text, date }) => {
 	return (
 		<div key={id} className="flex flex-row justify-start space-x-2 font-sans">
 			<div className="w-[32px] shrink-0">
-				<Image className="rounded-full" src="/twitter-profile.jpg" width={256} height={256} alt="@martinloretzzz" />
+				<Image
+					className="rounded-full"
+					src="/twitter-profile.jpg"
+					width={256}
+					height={256}
+					alt="@martinloretzzz"
+				/>
 			</div>
 			<div className="grow">
 				<a href="https://twitter.com/martinloretzzz" target="_blank">
-					<span className="font-bold">Martin Loretz</span>
-					<span className="text-gray-400 font-light"> @martinloretzzz · {date}</span>
+					<span className="text-gray-200 font-semibold">Martin Loretz</span>
+					<span className="text-gray-400 font-light"> · {date}</span>
 				</a>
 				<br />
 				<a href={`https://twitter.com/martinloretzzz/status/${id}`} target="_blank">
 					{text}
 				</a>
 			</div>
-			<a className="self-center" href={`https://twitter.com/intent/like?tweet_id=${id}`} target="_blank">
+			<a
+				className="self-center"
+				href={`https://twitter.com/intent/like?tweet_id=${id}`}
+				target="_blank"
+			>
 				<LikeIcon size={16} style={{ fill: "rgb(249, 24, 128)" }} />
 			</a>
 		</div>
